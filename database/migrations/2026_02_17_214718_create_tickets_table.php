9f005a0118b32cd339b7de7d31faaab90eff2eab
Jose Ivanildo <joseivanildomarques16@gmail.com>
2026-02-17 19:09:17 -0300

    (feat) Criando as Migrations do Sistema

---
+++ database/migrations/2026_02_17_214718_create_tickets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->foreignId('agent_id')->nullable()->constrained('users');
+            $table->enum('status', ['Aberto', 'Progresso', 'Resolvido'])->default('Aberto');
+            $table->enum('priority', ['baixa','media','urgente'])->default('baixa');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
